31327cd0bd9a7d2fbb72fe93a142b38651a33f07
Alexander Nevdakh <88935397+sashaPEn@users.noreply.github.com>
2022-08-21 18:17:03 +0300

    577

--- doczrc.js
+++ doczrc.js
@@ -4,6 +4,6 @@ const { menuItems } = require(`${projectRoot}/config/menu.json`);
 
 export default {
   files: 'docs/**/*.{md,markdown,mdx}',
-  title: 'Docz & Netlify CMS',
+  title: 'Учебные материалы 9Б',
   menu: menuItems
 }
